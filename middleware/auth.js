4ebb1a5164181ad4fa941a06789438b8d8c4c350
syaila <82511601+syailaananta@users.noreply.github.com>
2024-07-24 06:28:45 +0000

    17

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,18 +2,25 @@
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = 'arethisisajoke';
 
-const authenticateToken = (req, res, next) => {
-  const token = req.header('Authorization');
+const authenticate = async (req, res, next) => {
+  const token = req.header('Authorization').replace('Bearer ', '');
   if (!token) return res.status(401).send('Access denied. No token provided.');
 
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
-    req.user = decoded;
+    const userId = decoded.userId; // Assuming the payload contains the userId
+
+    const userRef = db.collection('users').doc(userId);
+    const userDoc = await userRef.get();
+    if (!userDoc.exists) return res.status(404).send('User not found.');
+
+    req.user = { id: userId, ...userDoc.data() };
     next();
   } catch (error) {
-    res.status(400).send('Invalid token.', error);
+    res.status(400).send('Invalid token.');
   }
 };
+
 
 
 const isLibrarian = (req, res, next) => {
